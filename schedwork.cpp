224a0bc6ab31d3ff28a64d9e62a9cd6cb594eb3d
William Clark <weclark@usc.edu>
2025-04-29 20:53:50 +0000

    Final pasted from local

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,8 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool fillSchedule(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftsWorked,
+    size_t currentDay,
+    size_t currentSlot
+    );
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -37,9 +44,61 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    if (avail.size() == 0 || avail[0].size() == 0) {
+        return false;
+    }
+
+    //clear sched
+    sched.clear();
+    sched.resize(avail.size());
+
+    //vector tracking shifts worked
+    vector<size_t> shiftsWorked(avail[0].size(), 0);
+
+    return fillSchedule(avail, dailyNeed, maxShifts, sched, shiftsWorked, 0, 0);
+
+}
+
+bool fillSchedule(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsWorked, size_t currentDay, size_t currentSlot){
+    
+    if (currentDay == avail.size()) {
+            return true;
+    }
+
+        //if enough workers booked continue next day
+    if (sched[currentDay].size() == dailyNeed) {
+        return fillSchedule(avail, dailyNeed, maxShifts, sched, shiftsWorked, currentDay + 1, 0);
+    }
 
 
+    for (Worker_T worker = 0; worker < avail[0].size(); worker++){
 
+        //loops through worker checks if available
+        if (avail[currentDay][worker]){
+            if (shiftsWorked[worker] < maxShifts){
+                
+                bool alreadyScheduledToday = false;
+                for (size_t i = 0; i < sched[currentDay].size(); i++){
+                    if (sched[currentDay][i] == worker){
+                        alreadyScheduledToday = true;
+                        break;
+                    }
+                }
 
+                if (!alreadyScheduledToday) {
+                    sched[currentDay].push_back(worker);
+                    shiftsWorked[worker]++;
+                    //try to fill next spot
+                    if (fillSchedule(avail, dailyNeed, maxShifts, sched, shiftsWorked, currentDay, currentSlot + 1)) {
+                        return true;
+                    }
+                    //backtrack if doesnt work
+                    sched[currentDay].pop_back();
+                    shiftsWorked[worker]--;
+                    }
+                }
+            }
+        }
+        return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void buildWords(std::string& curr,const std::string& in,std::string floating, size_t index, const std::set<std::string>& dict, std::set<std::string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,43 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> results;
+    if(in.empty()){ //handle empty input
+      return results;
+    }
 
+    std::string curr = in;
+    buildWords(curr, in, floating, 0, dict, results);
+    return results;
 }
 
 // Define any helper functions here
+void buildWords(std::string& curr, const std::string& in, std::string floating, size_t index, const std::set<std::string>& dict, std::set<std::string>& results){
+    if(index == in.size()){
+        if(floating.empty() && dict.find(curr) != dict.end()){
+            results.insert(curr);
+        }
+        return;
+  }
+
+  if(in[index] != '-') {
+    curr[index] = in[index];
+    buildWords(curr, in, floating, index + 1, dict, results);
+  }
+  else{
+    for(char letter = 'a'; letter <= 'z'; ++letter){
+      curr[index] = letter;
+      size_t pos = floating.find(letter);
+      if(pos != std::string::npos){
+        std::string newFloating = floating;
+        newFloating.erase(pos, 1);
+        buildWords(curr, in, newFloating, index + 1, dict, results);
+      }
+      else{
+        if(in.size() - index > floating.size()){
+          buildWords(curr, in, floating, index + 1, dict, results);
+        }
+      }
+    }
+  }
+}
